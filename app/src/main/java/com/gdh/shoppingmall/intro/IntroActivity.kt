7d922dec12b7f1962ad599407e70814b4f37f384
gumsu <chavi9@naver.com>
2021-03-20 13:32:36 +0900

    [#1] Intro 화면 생성

--- app/src/main/java/com/gdh/shoppingmall/intro/IntroActivity.kt
+++ app/src/main/java/com/gdh/shoppingmall/intro/IntroActivity.kt
@@ -1,7 +1,13 @@
 package com.gdh.shoppingmall.intro
 
 import android.app.Activity
+import android.os.Bundle
+import org.jetbrains.anko.setContentView
 
 class IntroActivity : Activity(){
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val ui = IntroActivityUI()
+        ui.setContentView(this)
+    }
 }
