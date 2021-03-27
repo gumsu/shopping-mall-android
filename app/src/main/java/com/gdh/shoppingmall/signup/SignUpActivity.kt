cdab8db4e740e395771be246824b337a455f7a5c
gumsu <chavi9@naver.com>
2021-03-27 18:07:49 +0900

    [#2] SignUp Activity 추가

---
+++ app/src/main/java/com/gdh/shoppingmall/signup/SignUpActivity.kt
@@ -0,0 +1,7 @@
+package com.gdh.shoppingmall.signup
+
+import net.codephobia.ankomvvm.components.BaseActivity
+
+class SignUpActivity : BaseActivity<SignUpViewModel>() {
+    override val viewModelType = SignUpViewModel::class
+}
