1e1e268b54fd219b735a9834f673e5eaf38df194
gumsu <chavi9@naver.com>
2021-03-27 19:17:47 +0900

    [#2] 회원 가입 UI 작성

---
+++ app/src/main/java/com/gdh/shoppingmall/signup/SignUpActivityUI.kt
@@ -0,0 +1,74 @@
+package com.gdh.shoppingmall.signup
+
+import android.graphics.Typeface
+import android.text.InputType
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.gdh.shoppingmall.R
+import net.codephobia.ankomvvm.databinding.bindString
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.textInputEditText
+import org.jetbrains.anko.design.textInputLayout
+import org.jetbrains.anko.sdk27.coroutines.onClick
+
+/**
+ * SignUpViewModel의 데이터에 의존적이기 때문에 생성자에서 주입받았다.
+ * ui.linearLayout -> 최상위 컨테이너
+ * bindString -> anko에서 제공하는 데이터 바인딩 함수, 서로의 값을 연결해 동기화
+ */
+
+class SignUpActivityUI(
+    private val viewModel: SignUpViewModel
+    ) : AnkoComponent<SignUpActivity> {
+    override fun createView(ui: AnkoContext<SignUpActivity>) =
+        ui.linearLayout {
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER_VERTICAL
+            padding = dip(20)
+
+            textView("회원가입") {
+                textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+                textSize = 20f
+                typeface = Typeface.DEFAULT_BOLD
+                textColor = R.color.design_default_color_primary
+            }.lparams(width = matchParent) {
+                bottomMargin = dip(50)
+            }
+
+            textInputLayout {
+                textInputEditText {
+                    hint = "Email"
+                    setSingleLine()
+                    bindString(ui.owner, viewModel.email)
+                }
+            }.lparams(width = matchParent) {
+                bottomMargin = dip(20)
+            }
+
+            textInputLayout {
+                textInputEditText {
+                    hint = "Name"
+                    setSingleLine()
+                    bindString(ui.owner, viewModel.name)
+                }
+            }.lparams(width = matchParent) {
+                bottomMargin = dip(20)
+            }
+
+            textInputLayout {
+                textInputEditText {
+                    hint = "Password"
+                    setSingleLine()
+                    inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                    bindString(ui.owner, viewModel.email)
+                }
+            }.lparams(width = matchParent) {
+                bottomMargin = dip(20)
+            }
+
+            button("회원가입") {
+                onClick { viewModel.signUp() }
+            }.lparams(width = matchParent)
+        }
+}
