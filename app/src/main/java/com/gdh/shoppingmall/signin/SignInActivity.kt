93d807cf69040e639d9f93080cef62fb128d7f09
gumsu <chavi9@naver.com>
2021-03-30 23:14:21 +0900

    [#3] SignIn Activity 추가

---
+++ app/src/main/java/com/gdh/shoppingmall/signin/SignInActivity.kt
@@ -0,0 +1,7 @@
+package com.gdh.shoppingmall.signin
+
+import net.codephobia.ankomvvm.components.BaseActivity
+
+class SignInActivity : BaseActivity<SignInViewModel>() {
+    override val viewModelType = SignInViewModel::class
+}
--- app/src/main/java/com/gdh/shoppingmall/signin/SignInViewModel.kt
+++ app/src/main/java/com/gdh/shoppingmall/signin/SignInViewModel.kt
@@ -4,6 +4,10 @@ import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import net.codephobia.ankomvvm.lifecycle.BaseViewModel
 
+/**
+ * 로그인에 필요한 email과 password 필드를 함께 초기화
+ */
+
 class SignInViewModel(app: Application) : BaseViewModel(app) {
     val email = MutableLiveData("")
     val password = MutableLiveData("")
