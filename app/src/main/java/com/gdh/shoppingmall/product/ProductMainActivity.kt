8e5765b686be7ead03685ba6ef78b0b74f6d4a69
gumsu <chavi9@naver.com>
2021-04-06 23:57:21 +0900

    [#7] 상품 메인 레이아웃 기본 클래스 추가

---
+++ app/src/main/java/com/gdh/shoppingmall/product/ProductMainActivity.kt
@@ -0,0 +1,13 @@
+package com.gdh.shoppingmall.product
+
+import android.os.Bundle
+import net.codephobia.ankomvvm.components.BaseActivity
+
+class ProductMainActivity : BaseActivity<ProductMainViewModel>() {
+
+    override val viewModelType = ProductMainViewModel::class
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/gdh/shoppingmall/product/ProductMainUI.kt
@@ -0,0 +1,14 @@
+package com.gdh.shoppingmall.product
+
+import android.view.View
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+
+class ProductMainUI(
+    private val viewModel: ProductMainViewModel
+) : AnkoComponent<ProductMainActivity> {
+
+    override fun createView(ui: AnkoContext<ProductMainActivity>): View {
+        TODO("Not yet implemented")
+    }
+}
