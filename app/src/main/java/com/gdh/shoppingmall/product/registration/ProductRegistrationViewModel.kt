3530937b66e372748fefaf6bb677550f45c6981b
gumsu <chavi9@naver.com>
2021-04-24 23:48:51 +0900

    [#8] 상품 등록 viewModel 생성

---
+++ app/src/main/java/com/gdh/shoppingmall/product/registration/ProductRegistrationViewModel.kt
@@ -0,0 +1,7 @@
+package com.gdh.shoppingmall.product.registration
+
+import android.app.Application
+import net.codephobia.ankomvvm.lifecycle.BaseViewModel
+
+class ProductRegistrationViewModel(app: Application) : BaseViewModel(app){
+}
