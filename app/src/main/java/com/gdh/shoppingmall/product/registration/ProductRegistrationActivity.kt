fc67a5d65d8bb75716700874c7b63a608c882222
gumsu <chavi9@naver.com>
2021-04-24 23:57:19 +0900

    [#8] 상품 등록 Activity 추가

---
+++ app/src/main/java/com/gdh/shoppingmall/product/registration/ProductRegistrationActivity.kt
@@ -0,0 +1,27 @@
+package com.gdh.shoppingmall.product.registration
+
+import android.os.Bundle
+import android.view.MenuItem
+import net.codephobia.ankomvvm.components.BaseActivity
+
+class ProductRegistrationActivity: BaseActivity<ProductRegistrationViewModel>() {
+
+    override val viewModelType = ProductRegistrationViewModel::class
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "상품 등록"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        item?.let {
+            when(item.itemId) {
+                android.R.id.home -> onBackPressed()    // 백버튼을 눌렀을 때와 같은 효과
+                else -> {}
+            }
+        }
+        return true
+    }
+}
