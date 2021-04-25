63776e6fe29df880311157097e9dc537418d332b
gumsu <chavi9@naver.com>
2021-04-25 18:05:33 +0900

    [#8] 이미지 업로드 로직 구현

--- app/src/main/java/com/gdh/shoppingmall/product/registration/ProductRegistrationViewModel.kt
+++ app/src/main/java/com/gdh/shoppingmall/product/registration/ProductRegistrationViewModel.kt
@@ -1,8 +1,14 @@
 package com.gdh.shoppingmall.product.registration
 
+import android.app.Activity.RESULT_OK
 import android.app.Application
+import android.content.Intent
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.gdh.shoppingmall.api.response.ApiResponse
+import com.gdh.shoppingmall.api.response.ProductImageUploadResponse
 import com.gdh.shoppingmall.product.category.categoryList
+import kotlinx.coroutines.launch
 import net.codephobia.ankomvvm.lifecycle.BaseViewModel
 
 /**
@@ -12,18 +18,21 @@ import net.codephobia.ankomvvm.lifecycle.BaseViewModel
  * categories -> 뷰에서 스피너로 보여줄 카테고리명 리스트를 담는 변수
  * categoryIdSelected -> 뷰의 스피너를 통해 선택된 카테고리의 아이디를 저장할 변수, 디폴트 값으로 첫 번째 카테고리 아이디 값 사용
  * ...length -> 뷰에 표시될 현재 각 텍스트의 길이(0/00) 형태, 데이터 바인딩 사용
+ *
+ * 이미지 등록 로직 구현
+ * Intent(Intent.ACTION_GET_CONTENT) -> 로컬 파일 시스템에서 특정 타입의 파일을 선택하는 액티비티 인텐트를 생성
  */
 
 class ProductRegistrationViewModel(app: Application) : BaseViewModel(app){
 
-    val imagesUrls: List<MutableLiveData<String?>> = listOf(
+    val imageUrls: List<MutableLiveData<String?>> = listOf(
         MutableLiveData(null as String?),
         MutableLiveData(null as String?),
         MutableLiveData(null as String?),
         MutableLiveData(null as String?)
     )
 
-    val imagesIds: MutableList<Long?> = mutableListOf(null, null, null, null)
+    val imageIds: MutableList<Long?> = mutableListOf(null, null, null, null)
 
     val productName = MutableLiveData("")
     val description = MutableLiveData("")
@@ -36,6 +45,8 @@ class ProductRegistrationViewModel(app: Application) : BaseViewModel(app){
 
     val productNameLength = MutableLiveData("0/$productNameLimit")
     val descriptionLength = MutableLiveData("0/$descriptionLimit")
+
+    var currentImageNum = 0
 
     fun checkProductNameLength() {
         productName.value?.let {
@@ -57,5 +68,45 @@ class ProductRegistrationViewModel(app: Application) : BaseViewModel(app){
 
     fun onCategorySelect(position: Int) {
         categoryIdSelected = categoryList[position].id
+    }
+
+    fun pickImage(imageNum: Int) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+
+        intent.resolveActivity(app.packageManager)?.let {
+            startActivityForResult(intent, REQUEST_PICK_IMAGES)
+        }
+        currentImageNum = imageNum
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != RESULT_OK) return
+
+        when (resultCode) {
+            REQUEST_PICK_IMAGES -> data?.let { uploadImage(it) }
+        }
+    }
+
+    private fun uploadImage(intent: Intent) =
+        getContent(intent.data)?.let { imageFile ->
+            viewModelScope.launch {
+                val response = ProductImageUploader().upload(imageFile)
+                onImageUploadResponse(response)
+            }
+        }
+
+    private fun onImageUploadResponse(response: ApiResponse<ProductImageUploadResponse>) {
+        if (response.success && response.data != null) {
+            imageUrls[currentImageNum].value = response.data.filePath
+            imageIds[currentImageNum] = response.data.productImageId
+        } else {
+            toast(response.message ?: "알 수 없는 오류가 발생했습니다.")
+        }
+    }
+
+    companion object {
+        const val REQUEST_PICK_IMAGES = 0
     }
 }
