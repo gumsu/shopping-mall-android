462415e8c51980c869d84ee8ad2e2f4d027256dc
gumsu <chavi9@naver.com>
2021-10-03 21:12:28 +0900

    [#14] 상품 검색 UI 작성

--- app/src/main/java/com/gdh/shoppingmall/product/search/ProductSearchViewModel.kt
+++ app/src/main/java/com/gdh/shoppingmall/product/search/ProductSearchViewModel.kt
@@ -7,6 +7,12 @@ import com.gdh.shoppingmall.product.list.ProductListItemDataSource
 import com.gdh.shoppingmall.product.list.ProductListPagedAdapter
 import net.codephobia.ankomvvm.lifecycle.BaseViewModel
 
+/**
+ * 검색 결과 페이지에서는 키워드 변경이 불가능하므로 keyword 변수를 String 형태로 가진다.
+ * LiveData<PagedList<T>> 는 ProductListViewModel에서 사용한 것을 재사용한다.
+ * 검색 목록에서는 ProductListItemDataSource를 만들 때 카테고리 값이 null이어야 하므로 null을 넘겨주고 키워드를 함께 넘겨준다.
+ */
+
 class ProductSearchViewModel(app: Application) : BaseViewModel(app),
     ProductListPagedAdapter.ProductLiveDataBuilder,
     ProductListPagedAdapter.OnItemClickListener {
