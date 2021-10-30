997d15e12d34375d0430ffccb4ab6373f18e9dfa
gumsu <chavi9@naver.com>
2021-10-30 23:21:08 +0900

    [#16] notification Id 프로퍼티 추가

--- app/src/main/java/com/gdh/shoppingmall/common/Prefs.kt
+++ app/src/main/java/com/gdh/shoppingmall/common/Prefs.kt
@@ -7,6 +7,8 @@ import com.gdh.shoppingmall.App
  * context를 파라미터로 넣고 객체를 생성해주어야 한다. context를 알고 있어야 하므로 사용 범위가 제한적이다.
  * 이 클래스를 싱클톤으로 만들고 App 클래스를 이용해 전역으로 사용 가능하다.
  * 어디서든 Prefs.name과 같은 코드로 값을 읽고 저장할 수 있다.
+ *
+ * 푸시 알림을 수신하기 위한 유니크한 키값 저장 필요
  */
 
 object Prefs {
@@ -15,6 +17,7 @@ object Prefs {
     private const val USER_NAME = "user_name"
     private const val USER_ID = "user_id"
     private const val FCM_TOKEN = "fcm-token"
+    private const val NOTIFICATION_ID = "notification-id"
 
     val prefs by lazy {
         PreferenceManager
@@ -49,5 +52,11 @@ object Prefs {
         get() = prefs.getString(FCM_TOKEN, null)
         set(value) = prefs.edit()
             .putString(FCM_TOKEN, value)
+            .apply()
+
+    var notificationId
+        get() = prefs.getInt(NOTIFICATION_ID, 0)
+        set(value) = prefs.edit()
+            .putInt(NOTIFICATION_ID, value)
             .apply()
 }
