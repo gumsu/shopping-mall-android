fda61a50e5d3dda3783832f293e6b19f2b99d180
gumsu <chavi9@naver.com>
2021-10-30 22:33:16 +0900

    [#16] 토큰 필드 추가

--- app/src/main/java/com/gdh/shoppingmall/api/request/SignInRequest.kt
+++ app/src/main/java/com/gdh/shoppingmall/api/request/SignInRequest.kt
@@ -1,10 +1,12 @@
 package com.gdh.shoppingmall.api.request
 
 import android.util.Patterns
+import com.gdh.shoppingmall.common.Prefs
 
 class SignInRequest(
     val email: String?,
-    val password: String?
+    val password: String?,
+    val fcmToken: String? = Prefs.fcmToken
 ) {
     fun isNotValidEmail() =
         email.isNullOrBlank() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()
